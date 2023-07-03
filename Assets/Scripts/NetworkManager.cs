6d54a3afc992d83974373c4185db4fc41e04af02
JanZinch <jan.zinchanka@mail.ru>
2023-07-03 14:30:00 +0300

    feat: lobby

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using UI;
+using UnityEngine;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
+
+public class NetworkManager : MonoBehaviour
+{
+    [SerializeField] private int _maxPlayers = 5;
+    [SerializeField] private LobbyForm _lobbyForm; 
+    
+    private Lobby _lobby;
+    private const float LobbyHeartbeatMax = 15.0f;
+    private float _lobbyHeartbeatTimer = LobbyHeartbeatMax;
+    
+    private void OnEnable()
+    {
+        _lobbyForm.OnCreateClick.AddListener(CreateLobby);
+        _lobbyForm.OnCreateClick.AddListener(JoinLobby);
+    }
+
+    private async void Start()
+    {
+        await UnityServices.InitializeAsync();
+
+        AuthenticationService.Instance.SignedIn += () =>
+        {
+            Debug.Log("Signed: " + AuthenticationService.Instance.PlayerId);
+        };
+
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+    }
+
+    private async void HandleLobbyHeartbeat()
+    {
+        if (_lobby != null)
+        {
+            _lobbyHeartbeatTimer -= Time.deltaTime;
+
+            if (_lobbyHeartbeatTimer <= 0.0f)
+            {
+                _lobbyHeartbeatTimer = LobbyHeartbeatMax;
+                await LobbyService.Instance.SendHeartbeatPingAsync(_lobby.Id);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        HandleLobbyHeartbeat();
+    }
+
+    private async void CreateLobby(string lobbyName)
+    {
+        try
+        {
+            _lobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName, _maxPlayers);
+
+            Debug.Log("Created: " + _lobby.Name + " " + _lobby.MaxPlayers);
+        }
+        catch (LobbyServiceException ex)
+        {
+            Debug.LogException(ex);
+        }
+        
+    }
+    
+    private async void JoinLobby(string lobbyName)
+    {
+        try
+        {
+            QueryLobbiesOptions queryOptions = new QueryLobbiesOptions()
+            {
+                Count = 1,
+                
+                Filters = new List<QueryFilter>()
+                {
+                    new QueryFilter(QueryFilter.FieldOptions.Name, lobbyName, QueryFilter.OpOptions.EQ),
+                }
+            };
+            
+            QueryResponse response = await Lobbies.Instance.QueryLobbiesAsync(queryOptions);
+
+            if (response.Results.Count == 0)
+            {
+                Debug.Log("Lobby not exists");
+            }
+            else
+            {
+                await Lobbies.Instance.JoinLobbyByIdAsync(response.Results[0].Id);
+                Debug.Log("Joined");
+            }
+        }
+        catch (LobbyServiceException ex)
+        {
+            Debug.LogException(ex);
+        }
+        
+    }
+    
+    private void OnDisable()
+    {
+        _lobbyForm.OnCreateClick.RemoveListener(CreateLobby);
+        _lobbyForm.OnCreateClick.RemoveListener(JoinLobby);
+    }
+
+}
---
+++ Assets/Scripts/UI/LobbyForm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class LobbyForm : MonoBehaviour
+    {
+        [SerializeField] private TMP_InputField _newLobbyName;
+        [SerializeField] private TMP_InputField _desiredLobbyName;
+        
+        [SerializeField] private Button _createLobby;
+        [SerializeField] private Button _joinTheLobby;
+
+        [SerializeField] private UnityEvent<string> _onCreateClick;
+        [SerializeField] private UnityEvent<string> _onJoinClick;
+        
+        public UnityEvent<string> OnCreateClick => _onCreateClick;
+        public UnityEvent<string> OnJoinClick => _onJoinClick;
+        
+        
+        private void OnEnable()
+        {
+            _createLobby.onClick.AddListener(CreateLobby);
+        }
+
+        private void CreateLobby()
+        {
+            _onCreateClick?.Invoke(_newLobbyName.text);
+        }
+        
+        private void OnDisable()
+        {
+            _createLobby.onClick.RemoveListener(CreateLobby);
+        }
+    }
+}
